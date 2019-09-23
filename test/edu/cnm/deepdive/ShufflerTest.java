6bf468b3b38c6bd19e3c0db8e5e5345e59cda726
rsena9 <resena133@gmail.com>
2019-09-23 12:17:46 -0600

    Initial commit

---
+++ test/edu/cnm/deepdive/ShufflerTest.java
@@ -0,0 +1,60 @@
+package edu.cnm.deepdive;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ShufflerTest {
+
+  @Test
+  void shuffle() {
+  }
+
+  @Test
+  void testShuffle() {
+  }
+
+  @Test
+  void testShuffle1() {
+  }
+
+  @Test
+  void testShuffle2() {
+  }
+
+  @Test
+  void testShuffle3() {
+  }
+
+  @Test
+  void testShuffle4() {
+  }
+
+  @Test
+  void testShuffle5() {
+  }
+
+  @Test
+  void testShuffle6() {
+  }
+
+  @Test
+  void testShuffle7() {
+  }
+
+  @Test
+  void testShuffle8() {
+  }
+
+  @Test
+  void testShuffle9() {
+  }
+
+  @Test
+  void testShuffle10() {
+  }
+
+  @Test
+  void testShuffle11() {
+  }
+}
